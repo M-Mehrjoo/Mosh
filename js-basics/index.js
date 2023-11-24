c95acc72b8b46a3dc5d868664f8e97e419c19389
Z.mehrjoo <z.mehrjoo@l-shop-team.de>
2023-11-24 13:03:10 +0100

    add js folder

---
+++ js-basics/index.js
@@ -0,0 +1,37 @@
+       // Geting Started
+       // This is my first JavaScript code!
+        console.log('Hello World');
+
+        // Basics: Variables
+        
+        // Command prompt: cmd
+        // Go back throu folders: cd..
+        //Go to a folder: cd folderName
+        
+        let name ='Mah';
+        console.log(name);
+
+                // Rules for naming Variables:
+                // Connot be a reserved keyword (if, esl, let, ...)
+                // Should be meaningful (a, jj, ...)
+                // Cannot start with a number (1name)
+                // Cannot contain a space or hyphen (firstName) *camelcase*
+                // Are case-sensitive (firstName ≠ FirstName)
+        
+        let firstName ='Mah';
+        let lastName ='Mehrjoo';
+        console.log(firstName)
+        console.log(lastName)
+
+        // Basics: Constants
+
+                // moteghayer
+        let interestRateM = 0.3;
+        interestRateM = 1;
+        console.log(interestRateM)
+
+                // Sabet
+        const interestRate = 0.3;
+        console.log(interestRate)
+
+        // Primitive Types
